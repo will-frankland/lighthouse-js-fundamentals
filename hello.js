65a1a5a3ec5e69145dd844791877b52c690e0d01
Will Frankland <will-frankland@hotmail.com>
2022-02-15 10:57:12 -0500

    hello.js tasks updated

--- hello.js
+++ hello.js
@@ -6,3 +6,18 @@ sayHello(); */
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('Will'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Jenny');
+console.log(greeting);
